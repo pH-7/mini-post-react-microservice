8c8e0fb4c17e908f0fab6616bc8d7b83fd8f9e6d
Pierre-Henry Soria <hi@ph7.me>
2020-09-10 22:54:17 +0930

    Send moderation pending comments into event bus

--- comments/index.js
+++ comments/index.js
@@ -48,8 +48,31 @@ app.post('/posts/:id/comments', async (req, res) => {
 
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log('Event Received: ', req.body.type);
+
+    const { type, data } = req.body;
+
+    if (type === 'CommentModerated') {
+        const { postId, id, status } = data;
+
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+        comment.status = status;
+
+        await axios.post('http://localhost:4005', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                status,
+                postId,
+                comment,
+            }
+        });
+    }
 
     res.send({});
 })
