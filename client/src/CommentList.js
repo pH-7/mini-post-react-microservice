601d29846934e75c929029827fc54ad67441b4c0
Pierre-Henry Soria <hi@ph7.me>
2020-09-16 22:37:41 +0930

    Show up when a comment is awaiting moderation

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 export default ({ comments }) => {
     const renderedComments = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>
+
+        const getContent = ((comment) => {
+            switch (comment.status) {
+                case 'approved':
+                    return comment.content;
+
+                case 'rejected':
+                    return 'This comment is awaiting moderation.';
+
+                case 'pending':
+                    return 'This comment is awaiting moderation.';
+
+                case 'rejected':
+                    return 'This comment has been rejected.';
+
+                default:
+                    return comment.content;
+
+            }
+        })();
+
+        return <li key={comment.id}>{getContent}</li>
     });
 
     return (
